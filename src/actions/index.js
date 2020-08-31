9e30957ecc533291dd15a01f31ffeab0e69ac9bd
empenguin1186 <emperorpenguint1186@gmail.com>
2020-08-31 22:23:14 +0900

    :+1: add actions

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+
+// define action creator
+export const increment = () => ({
+    type: INCREMENT
+});
+
+export const decrement = () => ({
+    type: DECREMENT
+});

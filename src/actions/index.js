ccd7e49e81bc6e3e6e7f9e86d7c41346a2ed2983
empenguin1186 <emperorpenguint1186@gmail.com>
2020-09-02 23:51:56 +0900

    :+1: add create form

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const READ_EVENTS = 'READ_EVENTS';
+export const CREATE_EVENT = 'CREATE_EVENT';
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1';
 const QUERY_STRING = '?token=token123';
 
@@ -9,4 +10,11 @@ export const readEvents = () => async dispatch => {
     const response = await axios.get(`${ROOT_URL}/events${QUERY_STRING}`);
     console.log(response)
     dispatch({ type: READ_EVENTS, response })
+}
+
+// define action creator
+export const postEvent = values => async dispatch => {
+    const response = await axios.post(`${ROOT_URL}/events${QUERY_STRING}`, values);
+    console.log(response)
+    dispatch({ type: CREATE_EVENT, response })
 }
